91e458b521aac6e68e7bea3af5a464c2fba2bb39
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-20 11:30:26 -0300

    Mobile navigation button click event

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const mobileNavButtonEl = document.querySelector(`.mobile-nav-button`);
+
+mobileNavButtonEl.addEventListener(`click`, () => {
+  const headerEl = document.querySelector("header");
+  headerEl.classList.toggle("nav-open");
+});
